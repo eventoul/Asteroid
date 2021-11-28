5382396bde68f439371b7dca316595d0bee8ea1f
eventoul <57504597+eventoul@users.noreply.github.com>
2021-11-28 23:18:59 +0200

    Add Rigidbody Component to the Player1. Update PlayerMovement.cs.
    Uncheck the Use Gravity.
    
    At PlayerMovement.cs add the methods to move the Plane.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,60 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float forceMagnitude;
+    [SerializeField] private float maxVelocity;
+    
     private Camera mainCamera;
-    
-    
+    private Rigidbody rb;
+
+    private Vector3 movementDirection;
+
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        TouchToMove();
+    }
+
+    private void FixedUpdate()
+    {
+        ForceToMove();
+    }
+
+    private void TouchToMove()
     {
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
             Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue(); // elegxos patimatos tis o8onis
 
             Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition); // metatroph twn syntetagmenwn apo tn analysh tis o8onis stis syntetagmenes toy world Unity.
-            Debug.Log(worldPosition);
+
+            movementDirection = transform.position - worldPosition;
+            movementDirection.z = 0;
+            movementDirection.Normalize();
+        }
+        else
+        {
+            movementDirection = Vector3.zero;
         }
     }
+    
+    private void ForceToMove()
+    {
+        if (movementDirection == Vector3.zero)
+        {
+            return;
+        }
+   
+        rb.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
+   
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+    } 
+    
 }
