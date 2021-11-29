beb2c8a69722528bc87743395054c226ebe6188a
eventoul <57504597+eventoul@users.noreply.github.com>
2021-11-30 00:40:00 +0200

    Update PlayerHealth.cs
    Add the Crash() method to deactivate the gameObject.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Crash()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gameObject.SetActive(false); // apenergopoihsh toy gameobejct. 
     }
 }
