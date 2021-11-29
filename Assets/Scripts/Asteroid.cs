ca7bd8d050f1e3582d173322a762318d2637ed18
eventoul <57504597+eventoul@users.noreply.github.com>
2021-11-30 00:45:30 +0200

    Update Asteroid.cs
    Add the functionality to Crash() the plane.

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -1,18 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
+        PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
 
-    // Update is called once per frame
-    void Update()
-    {
+        if (playerHealth == null)
+        {
+            return;
+        }
         
+        playerHealth.Crash();
     }
 }
