6f2b19358a4bc22c0c38901ebe7cc33821f3b617
eventoul <57504597+eventoul@users.noreply.github.com>
2021-12-01 00:41:53 +0200

    Update UIScript.cs
    Add variables for the names.

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -5,13 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class UIScript : MonoBehaviour
 {
+    private const string GAME_PLAY_SCENE = "GamePlay";
+    private const string MENU_SCENE = "Menu";
+    
     public void StartGame()
     {
-        SceneManager.LoadScene("GamePlay");
+        SceneManager.LoadScene(GAME_PLAY_SCENE);
     }
 
     public void ReturnToMenu()
     {
-        SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene(MENU_SCENE);
     }
 }
